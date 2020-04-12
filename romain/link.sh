50031fa1774de007cded2af83bc9fb8573de427d
Romain Roehrig <romain.roehrig@gmail.com>
2020-04-12 15:06:26 +0200

    Update Romain's part

--- romain/link.sh
+++ romain/link.sh
@@ -10,5 +10,5 @@ mkdir tmp
 
 for i in `seq -w 00 10`
 do
-  ln -s $DIR0/images//DELANNOY_slab_cont_WA_evap$i/yearavg//xy_$var.png $DIR0/tmp/xy_${var}_evap$i.png
+  ln -s $DIR0/images//DELANNOY_slab_cont_WA_evap$i/JJASavg//xy_$var.png $DIR0/tmp/xy_${var}_evap$i.png
 done
